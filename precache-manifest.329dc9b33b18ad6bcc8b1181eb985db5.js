4491e536c4fa93b2e409ebd8297bca6ac9aa850b
Paul Rutter <paul@blueconic.com>
2020-05-07 10:36:14 +0200

    Add built weather app

---
+++ precache-manifest.329dc9b33b18ad6bcc8b1181eb985db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52032c81f0bbaa849cc21ddd618b2d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998941438e351f88eb8c",
+    "url": "/static/css/main.cf45a44f.chunk.css"
+  },
+  {
+    "revision": "6fcb1fe2831f20af6d07",
+    "url": "/static/js/2.be189f37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.be189f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998941438e351f88eb8c",
+    "url": "/static/js/main.ea284891.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
